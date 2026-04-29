6d288d64c8cfb0a300ecacda6d4fac99977c433c
Dharti-git <dhartikharapkar2021@gmail.com>
2026-04-29 08:58:19 +0530

    Initial Docker project commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+mongoose
+  .connect("mongodb://mongodb:27017/mydb")
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Docker app is working");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
